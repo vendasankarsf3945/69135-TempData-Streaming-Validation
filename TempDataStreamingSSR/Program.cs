378631ed3b9bc52954596432c852f99d6d981c98
vendasankarsf3945 <venda.sankar@syncfusion.com>
2026-09-11 17:37:30 +0530

    Added the sample for the issue #69135

---
+++ TempDataStreamingSSR/Program.cs
@@ -0,0 +1,39 @@
+using TempDataStreamingSSR.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Logging.AddConsole();
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
+builder.Logging.AddFilter("Microsoft.AspNetCore.Components", LogLevel.Warning);
+builder.Logging.AddFilter("TempDataStreamingSSR.Components", LogLevel.Information);
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(20);
+});
+builder.Services.AddRazorComponents(options =>
+{
+    options.TempDataCookie.Name = ".AspNetCore.Components.TempData";
+    options.TempDataCookie.HttpOnly = true;
+    options.TempDataCookie.SameSite = SameSiteMode.Strict;
+    options.TempDataCookie.SecurePolicy = CookieSecurePolicy.None;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+app.UseSession();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>();
+
+app.Run();
